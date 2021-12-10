b5f3ba3ec0208fad023e193fef4900024909562d
sandeepkumarshukla <sandeep.shukla@gmail.com>
2021-12-10 15:33:15 +0100

    Initial commit

---
+++ src/main/java/com/springoot/practice/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -0,0 +1,15 @@
+package com.springoot.practice.springsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtApplication {
+
+	//https://www.youtube.com/watch?v=X80nJ5T7YpE
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springoot/practice/springsecurityjwt/jwt/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.springoot.practice.springsecurityjwt.jwt;
+
+public class AuthenticationResponse {
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
