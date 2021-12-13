afe6256d6621777ee964d3cdf225652bf01ce445
sandeepkumarshukla <sandeep.shukla@gmail.com>
2021-12-13 09:21:27 +0100

    for java 17 we must include jaxb as seperate dependency.

--- src/main/java/com/springoot/practice/springsecurityjwt/SpringSecurityJwtApplication.java
+++ src/main/java/com/springoot/practice/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSecurityJwtApplication {
 
 	//https://www.youtube.com/watch?v=X80nJ5T7YpE
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJwtApplication.class, args);
 	}
