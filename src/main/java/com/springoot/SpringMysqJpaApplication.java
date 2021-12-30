f3a165516e5dec2def66e2a8161ac8b311fe24ed
sandeepkumarshukla <sandeep.shukla@gmail.com>
2021-12-30 07:31:35 +0100

    Main clas name changed to keep consistency

--- src/main/java/com/springoot/SpringMysqJpaApplication.java
+++ src/main/java/com/springoot/SpringMysqJpaApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityJwtApplication {
+public class SpringMysqJpaApplication {
 
 	//https://www.youtube.com/watch?v=X80nJ5T7YpE
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+		SpringApplication.run(SpringMysqJpaApplication.class, args);
 	}
 
 }
